6dc56372624d6be2bf332ced6247d3607b617b2a
罗鹏 <luopen@123.com>
2019-11-22 00:41:25 +0800

    init

---
+++ src/pages/Home.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <van-sticky>
+            <van-nav-bar title="九山" >
+                <van-icon name="search" slot="right" />
+            </van-nav-bar>
+        </van-sticky>
+        <van-swipe style="height: 200px;" :autoplay="3000">
+            <van-swipe-item v-for="(image, index) in images" :key="index">
+                <img :src="image" style="height: 200px;width: 100%"/>
+            </van-swipe-item>
+        </van-swipe>
+
+        <van-grid :colum-num="4">
+            <van-grid-item icon="photo-o" :text="bean.title" v-for="(bean,index) in spuList" :key="index">
+                <van-image style="height: 80px"  slot="icon" :src="bean.url" fit="cover" round />
+            </van-grid-item>
+
+        </van-grid>
+        <!--<p>极品豪车</p>-->
+        <div class="teaweb_title">热销榜</div>
+        <van-grid :column-num="3">
+            <van-grid-item
+                    v-for="(item,index) in hotList"
+                    :key="index"
+                    icon="photo-o"
+                    :text="item.title">
+                <van-image style="height: 80px;width: 80px"  slot="icon" :src="item.url" fit="scale-down"  />
+                <div slot="text" style="font-size: 10px;flex-direction: column;justify-content: center;display: flex">
+
+                    <span>{{item.title}}</span>
+                    <span>{{item.price}}</span>
+                </div>
+            </van-grid-item>
+
+        </van-grid>
+
+        <!--<p>精品茶具</p>-->
+        <div class="teaweb_title">精品推荐</div>
+        <van-grid :border="false" :column-num="3">
+            <van-grid-item v-for="(image,index) in images" :key="index" text="尽请期待">
+                <van-image :src="image" style="height: 80px" fit="cover" slot="icon" />
+            </van-grid-item>
+        </van-grid>
+
+        <div class="teaweb_title">礼品必备</div>
+        <van-grid :border="false" :column-num="3">
+            <van-grid-item v-for="(image,index) in images" :key="index" text="尽请期待">
+                <van-image :src="image" style="height: 80px" fit="cover" slot="icon"/>
+            </van-grid-item>
+        </van-grid>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data() {
+            return {
+                images: [
+                    'https://www.93goodtea.com/img/tea/tea25.jpg',
+                    'https://www.93goodtea.com/img/pic/775x362-2.jpg',
+                    'https://www.93goodtea.com/img/pic/775x362.jpg',
+
+                ],
+                spuList:[
+                    {title:"云雾毛尖",url:"https://www.93goodtea.com/img/pic/504x567%E5%89%AF%E6%9C%AC.jpg"},
+                    {title:"手工茶",url:"https://www.93goodtea.com/img/pic/421x273%E5%89%AF%E6%9C%AC.jpg"},
+                    {title:"明前茶",url:"https://www.93goodtea.com/img/pic/354x271%E5%89%AF%E6%9C%AC.jpg"},
+                    {title:"谷雨茶",url:"https://www.93goodtea.com/img/pic/271x271.jpg"},
+                ],
+                hotList:[
+                    {title:"精品贴合",url:"https://www.93goodtea.com/img/tea/tea63.jpg",price:"10元/只"},
+                    {title:"精品礼盒",url:"https://www.93goodtea.com/img/tea/tea22.jpg",price:"40元/只"},
+                    {title:"明前二级次芽",url:"https://www.93goodtea.com/img/tea/tea53.jpg",price:"560元/500克"},
+                    {title:"明前一级包芽",url:"https://www.93goodtea.com/img/tea/tea53.jpg",price:"880元/500克"},
+                    {title:"明前特级珍芽",url:"https://www.93goodtea.com/img/tea/tea20.jpg",price:"1260元/500克"},
+                    {title:"明前特级顶芽",url:"https://www.93goodtea.com/img/pic/421x273%E5%89%AF%E6%9C%AC.jpg",price:"1688元/500克"},
+                ]
+            };
+        },
+        methods:{
+            goDetail(index){
+                this.$router.push('goodsDetail')
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/pages/MoutainList.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <van-sticky>
+            <van-nav-bar title="大别山脉">
+            </van-nav-bar>
+        </van-sticky>
+        <div>
+            <van-cell
+                    v-for="item in list"
+                    :key="item"
+
+
+            >
+                <van-image :src="item.url" style="height: 200px;width: 100%" fit="cover" />
+                <p style="text-align: center">{{item.title}}</p>
+
+            </van-cell>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'goods',
+        components: {},
+        data() {
+            return {
+                list: [{"url":"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=900135045,1976899860&fm=26&gp=0.jpg",title:"鸡公山"},
+                    {"url":"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2944906482,1620656057&fm=26&gp=0.jpg",title:"车云山"},
+                    {"url":"https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=7bf99062f21f3a294ec5dd9cf84cd754/0b55b319ebc4b745b40486aacdfc1e178b8215fd.jpg",title:"震雷山"},
+                    {"url":"https://gss2.bdstatic.com/9fo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=caad5d1d963df8dcb23087c3ac7819ee/f3d3572c11dfa9ec307105fd6bd0f703908fc1f9.jpg",title:"黑龙潭"},
+                    {"url":"https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike180%2C5%2C5%2C180%2C60/sign=181620a3be3eb13550cabfe9c777c3b6/8694a4c27d1ed21b96e822aeaf6eddc451da3f53.jpg",title:"灵山"},
+                    {"url":"https://www.93goodtea.com/img/tea/tea25.jpg",title:"皇城山"}],
+                loading: false,
+                finished: true
+            };
+        },
+        methods: {
+            onLoad() {
+                // 异步更新数据
+//                setTimeout(() => {
+//                    for (let i = 0; i < 10; i++) {
+//                        this.list.push(this.list.length + 1);
+//                    }
+//                    // 加载状态结束
+//                    this.loading = false;
+//
+//                    // 数据全部加载完成
+//                    if (this.list.length >= 40) {
+//                        this.finished = true;
+//                    }
+//                }, 500);
+            },
+            gotoGoodsDetail(){
+                this.$router.push('goodsDetail')
+            }
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+
+</style>
